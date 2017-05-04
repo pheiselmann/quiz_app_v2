8a3063a2a2b270cee2e04225c60b9fac52093fbc
pheiselmann <prhmann@gmail.com>
2017-05-03 23:33:14 -0700

    It ain't pretty, but it works

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var state = {
 	questionCount:[1],
     correctAnswers:[],
     incorrectAnswers: [],
+    //feedback: ["", "Correct!", "Incorrect, the correct answer is " + this.questions.correctAnswer + "."],
 	questions: [
 		{
 		question: "Which of these Americans did not win a Nobel Prize in Literature?" , 
@@ -48,7 +49,7 @@ var state = {
 		question: "Which of these American musical groups was founded by Lou Reed?", 
 		answer1: "The Beach Boys",
 		answer2: "The Eagles",
-		answer3: "The Velvet Undergound",
+		answer3: "The Velvet Underground",
 		answer4: "Flipper",
 		correctAnswer: "The Velvet Underground"
 		}
@@ -57,31 +58,30 @@ var state = {
 
 var questionTemplate = (
 	'<form class="js-quiz-form" id="js-quiz-form">' +
+	    '<span class="js-answer-feedback"></span>' +
 	    '<span class="js-question-count"></span><br>' +
+	    '<span class="js-quiz-score"></span><br>' +
 		'<span class="js-question"></span><br>' +
-      	'<input type="radio" name="js-answer1" required value="js-answer1" >' + 
-      	'<span class="js-answer_1"></span><br>' +
-      	'<input type="radio" name="js-answer2" value="js-answer2" >' +
-      	'<span class="js-answer_2"></span><br>' +
-        '<input type="radio" name="js-answer3" value="js-answer3" >' +
-        '<span class="js-answer_3"></span><br>' +
-        '<input type="radio" name="js-answer4" value="js-answer4" >' +
-        '<span class="js-answer_4"></span><br>' +
-        '<button class="js-submit" type="submit" value="submit">Submit Answer</button>' +
-    '</form>'
+      	'<input class="radio" type="radio" name="js-answer1" required value="js-answer1" >' + 
+      	'<span class="js-answer_1" id="answerId"></span><br>' +
+      	'<input class="radio" type="radio" name="js-answer2" value="js-answer2" >' +
+      	'<span class="js-answer_2" id="answerId"></span><br>' +
+        '<input class="radio" type="radio" name="js-answer3" value="js-answer3" >' +
+        '<span class="js-answer_3" id="answerId"></span><br>' +
+        '<input class="radio" type="radio" name="js-answer4" value="js-answer4" >' +
+        '<span class="js-answer_4" id="answerId"></span><br>' +
+        '<button class="js-submit" type="submit" value="submit">Submit Answer</button><br>' +
+        
+        // '<form class="js-next-question" id="js-next-question">' +
+        // 	'<button class="js-submit" type="submit" value="submit">Next Question</button><br>' +
+        // '</form>' +
+    '</form>' 
+    
 );
 
-var questionCountTemplate = (
-    '<span class="js-question-counter"></span>'
-);
-
-
-var correctAnswers = 0;
-
-var incorrectAnswers = 0;
-	
-var questionIndex = 0;
-
+// var questionCountTemplate = (
+//     '<span class="js-question-counter"></span>'
+// );
 
 
 function displayCurrentScore(correctAnswers, incorrectAnswers) {
@@ -89,13 +89,15 @@ function displayCurrentScore(correctAnswers, incorrectAnswers) {
 
 }
 
-function startQuiz(formElement, questionCountElement, questionCountTemplate, questionTemplate, state) {
+function startQuiz(formElement, questionCountElement, questionTemplate, state) {
   $('#js-start-quiz').click(function(event) {
     event.preventDefault();
 	$(this).addClass("hidden");
+
     //$('.js-test').text(state.questions[0].correctAnswer);
-    var questionIndex = 0;
-    renderQuestion(formElement, questionTemplate, questionIndex, state);
+    var questionIndex = state.questionCount.length - 1;
+    var feedback = 0;
+    renderQuestion(formElement, questionTemplate, questionIndex, state, feedback);
     //renderQuestionCount(questionCountElement, questionCountTemplate, state);
     
   });
@@ -110,22 +112,106 @@ function startQuiz(formElement, questionCountElement, questionCountTemplate, que
 //    questionCountElement.html(element);
 //  }
 
-function renderQuestion(formElement, questionTemplate, questionIndex, state) {
+function renderQuestion(formElement, questionTemplate, questionIndex, state, feedback) {
 
 	var element = $(questionTemplate);
 
 	element.find('.js-question-count').text("Question " + state.questionCount.length +
 		" out of 5");
+	element.find('.js-quiz-score').text("Score: " + state.correctAnswers.length + " Correct, " +
+		state.incorrectAnswers.length + " Incorrect");
 	element.find('.js-question').text(state.questions[questionIndex].question);
 	element.find('.js-answer_1').text(state.questions[questionIndex].answer1);
 	element.find('.js-answer_2').text(state.questions[questionIndex].answer2);
 	element.find('.js-answer_3').text(state.questions[questionIndex].answer3);
 	element.find('.js-answer_4').text(state.questions[questionIndex].answer4);
 
+	var feedbackMsg = feedback;
+
+	if (feedbackMsg === 0) {
+		element.find('.js-answer-feedback').text("");
+	}
+	else if (feedbackMsg === 1) {
+		element.find('.js-answer-feedback').text("Correct!");
+
+	}
+	else if (feedbackMsg === 2) {
+		element.find('.js-answer-feedback').text("Incorrect!  The correct answer is \"" + 
+			state.questions[questionIndex].correctAnswer + "\".");
+	}
+
 	formElement.html(element);
     //$('.js-display-question').html(element);
 	//render question, update score, and update question count
 
+}
+
+function renderFeedback(formElement, questionTemplate, questionIndex, state, feedback) {
+	var element = $(questionTemplate);
+
+	//element.find('.js-question-count').text("Question " + state.questionCount.length +
+	//	" out of 5");
+	// element.find('.js-quiz-score').text("Score: " + state.correctAnswers.length + " Correct, " +
+	// 	state.incorrectAnswers.length + " Incorrect");
+	element.find('.radio').addClass("hidden");
+	element.find('.js-submit').addClass('hidden');
+	//element.find('.js-question').text(state.questions[questionIndex].question);
+	//element.find('.js-answer_1').text(state.questions[questionIndex].answer1);
+	//element.find('.js-answer_2').text(state.questions[questionIndex].answer2);
+	//element.find('.js-answer_3').text(state.questions[questionIndex].answer3);
+	//element.find('.js-answer_4').text(state.questions[questionIndex].answer4);
+
+	var feedbackMsg = feedback;
+
+	if (feedbackMsg === 0) {
+		element.find('.js-answer-feedback').text("");
+	}
+	else if (feedbackMsg === 1) {
+		element.find('.js-answer-feedback').text("Correct!");
+
+	}
+	else if (feedbackMsg === 2) {
+		element.find('.js-answer-feedback').text("Incorrect!  The correct answer is \"" + 
+			state.questions[questionIndex].correctAnswer + "\".");
+	}
+
+	formElement.html(element);
+    //$('.js-display-question').html(element);
+	//render question, update score, and update question count
+}
+
+function renderFinalScore(formElement, questionTemplate, questionIndex, state, feedback) {
+	var element = $(questionTemplate);
+
+	//element.find('.js-question-count').text("Question " + state.questionCount.length +
+	//	" out of 5");
+	element.find('.js-quiz-score').text("Score: " + state.correctAnswers.length + " Correct, " +
+		state.incorrectAnswers.length + " Incorrect");
+	element.find('.radio').addClass("hidden");
+	element.find('.js-submit').addClass('hidden');
+	//element.find('.js-question').text(state.questions[questionIndex].question);
+	//element.find('.js-answer_1').text(state.questions[questionIndex].answer1);
+	//element.find('.js-answer_2').text(state.questions[questionIndex].answer2);
+	//element.find('.js-answer_3').text(state.questions[questionIndex].answer3);
+	//element.find('.js-answer_4').text(state.questions[questionIndex].answer4);
+
+	// var feedbackMsg = feedback;
+
+	// if (feedbackMsg === 0) {
+	// 	element.find('.js-answer-feedback').text("");
+	// }
+	// else if (feedbackMsg === 1) {
+	// 	element.find('.js-answer-feedback').text("Correct!");
+
+	// }
+	// else if (feedbackMsg === 2) {
+	// 	element.find('.js-answer-feedback').text("Incorrect!  The correct answer is \"" + 
+	// 		state.questions[questionIndex].correctAnswer + "\".");
+	// }
+
+	formElement.html(element);
+    //$('.js-display-question').html(element);
+	//render question, update score, and update question count
 }
 
 
@@ -134,10 +220,32 @@ function handleSubmitAnswer(formElement, state) {
 		event.preventDefault();
 		var answer = formElement.find("input[type='radio']:checked").val();
         //compare answer (which is 'value') to answer in state and then to correctAnswer
-        var answerNumber = answer.slice(-1);
+        var answerNumber = "answer" + answer.slice(-1);
+        var questionIndex = state.questionCount.length - 1;
+        var feedback;
+
+        if (state.questions[questionIndex][answerNumber] === state.questions[questionIndex].correctAnswer) {
+        	feedback = 1;
+        	incrementCorrectAnswers(state);
+        	renderFeedback(formElement, questionTemplate, questionIndex, state, feedback);
+        	//alert ("Correct!");
+        } else {
+        	feedback = 2;
+        	incrementIncorrectAnswers(state);
+        	renderFeedback(formElement, questionTemplate, questionIndex, state, feedback);
+        }
+        //else alert("Wrong!");
+
+        if (state.questionCount.length < 5) {
+        	$('.js-next-question').removeClass("hidden");
+        } else {
+        	renderFinalScore(formElement, questionTemplate, questionIndex, state, feedback);
+        	$('.js-play-again').removeClass('hidden');
+        }
+
          
         //$('js-test').text(answer);
-		alert("Value of Submitted Radio is: " + answer);
+		//alert("Value of Submitted Radio is: " + state.questions[questionIndex][answerNumber]);
 		// check answer, give feedback, render next item/score/question#
 		//displayNextQuestionButton(); // show "Next Question" button
 	});
@@ -145,8 +253,47 @@ function handleSubmitAnswer(formElement, state) {
 
 }
 
-function incrementStateCounter(state) {
+function playAgain(formElement, questionCountElement, questionTemplate, state) {
+	$('#js-play-again').click(function(event) {
+    event.preventDefault();
+	$(this).addClass("hidden");
+
+	state.questionCount = [1];
+	state.correctAnswers = [];
+	state.incorrectAnswers = [];
+
+	var questionIndex = state.questionCount.length - 1;
+	var feedback = 0;
+    renderQuestion(formElement, questionTemplate, questionIndex, state, feedback);
+    });
+}
+
+function nextQuestion(formElement, questionCountElement, questionTemplate, state) {
+	$('#js-next-question').click(function(event) {
+    event.preventDefault();
+	$(this).addClass("hidden");
+
+    //$('.js-test').text(state.questions[0].correctAnswer);
+    incrementQuestionCount(state);
+    var questionIndex = state.questionCount.length - 1;
+    var feedback = 0;
+    renderQuestion(formElement, questionTemplate, questionIndex, state, feedback);
+    //renderQuestionCount(questionCountElement, questionCountTemplate, state);
+    
+  });
+}
+
+
+function incrementQuestionCount(state) {
 	state.questionCount.push(1);	
+}
+
+function incrementCorrectAnswers(state) {
+	state.correctAnswers.push(1);
+}
+
+function incrementIncorrectAnswers(state) {
+	state.incorrectAnswers.push(1);
 }
 
 function hideItem(itemClass) {
@@ -160,6 +307,7 @@ function hideItem(itemClass) {
 $(function() {
 	var questionCountElement = $('.js-question-count');
 	var formElement = $('.js-display-question');
-	startQuiz(formElement, questionCountElement, questionCountTemplate, questionTemplate, state);
+	nextQuestion(formElement, questionCountElement, questionTemplate, state);
+	startQuiz(formElement, questionCountElement, questionTemplate, state);
     handleSubmitAnswer(formElement, state);
 });
